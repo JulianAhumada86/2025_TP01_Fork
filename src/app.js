9956a91f9d99f49d3c814b52251e1ffa361e7d20
Julian Ahumada <2214335@ucc.edu.ar>
2025-08-26 21:27:57 -0300

    Agregar el buenas tardes mundo, y su documentacion

--- src/app.js
+++ src/app.js
@@ -6,4 +6,11 @@ function saludar() {
 function despedir(){
     console.log("Chau mundo")
 }
+
+function buenasTardes(){
+    console.log("Buenas tardes mundo")
+}
+
 saludar();
+buenasTardes();
+despedir();
